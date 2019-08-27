b833a2d460f1fc4386667044c1db27c319e45132
Lucas Fernandez Nicolau <dev.lucasfn@icloud.com>
2019-08-27 20:42:06 -0300

    Correção em constraints da SegmentedStackView

--- MarketAccessibility/View/MoneyInputView.swift
+++ MarketAccessibility/View/MoneyInputView.swift
@@ -81,7 +81,7 @@ class MoneyInputView: UIView {
             moneyCollectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -46),
             
             segmentedStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            segmentedStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16),
+            segmentedStackView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -16),
             segmentedStackView.heightAnchor.constraint(equalToConstant: 30),
             segmentedStackView.widthAnchor.constraint(equalToConstant: 150),
             

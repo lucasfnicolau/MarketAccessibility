46102188482556c04cf1bb3087ed11665b5cb3d0
raxanob <rayanexavier161@gmail.com>
2019-08-30 18:52:24 -0300

    pagina animationVC funcionando

--- MarketAccessibility/Controller/AnimationVC.swift
+++ MarketAccessibility/Controller/AnimationVC.swift
@@ -4,55 +4,51 @@
 //
 //  Created by Rayane Xavier on 30/08/19.
 //  Copyright © 2019 Lucas Fernandez Nicolau. All rights reserved.
-//
+// swiftlint:disable trailing_whitespace
 
 import UIKit
 
-//class AnimationVC: UIViewController {
-//
-//    @IBOutlet weak var animationOutlet: UIImageView!
-//    var animationList:[UIImage] = []
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.view.backgroundColor = UIColor.App.money
-//        
-//        animationScene()
-//    }
-//    func animationScene() {
-//        for counter in 1...6 {
-//            animationList.append( UIImage(named: "Andando\(6-counter)")!)
-//        }
-//
-//        for counter in 0...5 {
-//            animationList.append(UIImage(named: "Andando\(counter)")!)
-//        }
-//    }
-//
-//    @objc func animationImageSelected() {
-//        animationOutlet.animationImages = animationList
-//        animationOutlet.animationDuration = 2
-//        animationOutlet.animationRepeatCount = 1
-//
-//        animationOutlet.startAnimating()
-//    }
-//
-//    func setConstrains() {
-//        animationOutlet.translatesAutoresizingMaskIntoConstraints = false
-//
-//        NSLayoutConstraint.activate([
-//            animationOutlet.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-//            animationOutlet.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-//            ])
-//    }
-//    /*
-    // MARK: - Navigation
+class AnimationVC: UIViewController {
 
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
+    @IBOutlet weak var animationOutlet: UIImageView!
+    var animationList: [UIImage] = []
+    var img = UIImage(named: "Andando1.png")
 
-//}
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.App.money
+
+        animationScene()
+        animationImageSelected()
+        setConstrains()
+    }
+    
+        func animationScene() {
+            
+            for counter in 1...6 {
+                guard let image = UIImage(named: "Andando\(counter)") else { return }
+                animationList.append(image)
+            }
+            
+            for counter in (1...6).reversed() {
+                guard let image = UIImage(named: "Andando\(counter)") else { return }
+                animationList.append(image)
+            }
+        }
+
+        func animationImageSelected() {
+            animationOutlet.animationImages = animationList
+            animationOutlet.animationDuration = 1.25
+            animationOutlet.animationRepeatCount = -1
+
+            animationOutlet.startAnimating()
+        }
+
+        func setConstrains() {
+            animationOutlet.translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                animationOutlet.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+                animationOutlet.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+                ])
+    }
+}
